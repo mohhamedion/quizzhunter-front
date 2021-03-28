64e9f57a936397ed87e77fe89a8bf1b41fb98af8
Mohamad <mohhamedion@gmail.com>
2021-03-28 17:10:54 +0300

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  serverMiddleware: ["redirect-ssl"],
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - quizzhunter',
